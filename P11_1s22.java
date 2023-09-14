f3e824c5ca95692857e72184d3ccccd20b664e09
AlexJaviernot <144191141+AlexJaviernot@users.noreply.github.com>
2023-09-13 19:31:23 -0600

    Add files via upload

---
+++ P11_1s22.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class P11_1s22{
+public static void main(String[] args){
+Scanner teclado = new Scanner(System.in);
+int edad;
+
+  System.out.println("Ingresa tu edad tu edad");
+ edad = teclado.nextInt();
+
+  if (edad >= 18){
+   System.out.println("Eres mayor de edad");
+
+ }else{
+System.out.println("No eres mayor de edad");
+
+System.ou.println("Raiz cuadrada de 10"+
+Math.sqrt(10));
+System.out.println("Potencia"+ Math.pw(5,3));
+System.out.println("Residuo de 10/2: " +(10%2));
+System.out.println("Residuo de 11/2: " +(11%2));
+  }
+  }
+}
---
+++ P12_1s22.java
@@ -0,0 +1,9 @@
+public class P10_1s22{
+public static void main(String[] args){
+int a = 4;
+int b = 8;
+if (a<b){
+System.out.println("Por que aparezco");
+   }
+java}
+}
